35ae58eba0fb13417b5b6d57ef70ea07ca7f473c
Surajit Shaw <piklu3043@gmail.com>
2025-05-25 18:17:44 +0530

    collection view setup and data source added

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -9,7 +9,19 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
+    
+    
     var username: String!
+    var followes: [Follower] = [
+        
+    ]
+    
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,10 +42,13 @@ class FollowerListVC: UIViewController {
             case .failure(let error):
                 self.showAlert(title: "Bad Request", message: error.rawValue)
             case .success(let followers):
-                print("No of followes: \(followers.count)")
-                print(followers)
+                self.followes = followers
+                self.updateData()
             }
         }
+        
+        configureCollectionView()
+        configureDataSource()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -42,5 +57,42 @@ class FollowerListVC: UIViewController {
         navigationController?.isNavigationBarHidden = false
     }
     
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        view.addSubview(collectionView)
+                collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
     
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding:CGFloat = 12
+        let minItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([
+            .main
+        ])
+        snapshot.appendItems(followes)
+        DispatchQueue.main.async{
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+    }
 }
